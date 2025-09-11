96a9b4077939e69bd29032a9cd2bc3673f05cfc8
Rahul Choudhary <rahulchoudhary@Rahuls-MacBook-Pro.local>
2025-09-11 09:52:36 +0530

    Added Persist to retain the states

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 import type { TaskStore } from './types';
 import { initialTaskState } from './state';
 import { createTaskActions } from './actions';
 
 export const useTaskStore = create<TaskStore>()(
   devtools(
+    persist(
       (...args) => ({
         ...initialTaskState,
         ...createTaskActions(...args)
-      })
+      }),
+      {
+        name: 'task-store', // name of the item in storage
+        partialize: (state) => ({
+          tasks: state.tasks,
+          isLoading: state.isLoading
+        }),
+      }
+    )
   )
 );
 
